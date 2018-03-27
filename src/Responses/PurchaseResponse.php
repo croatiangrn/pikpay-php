6308b2085ed80cb8e65daaf8eeb49d9939b70e64
Marko Mikulic <mikulic.marko@spark.ba>
2018-03-27 16:18:40 +0200

    Added 3d validation method

--- src/Responses/PurchaseResponse.php
+++ src/Responses/PurchaseResponse.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace SelimSalihovic\PikPay\Responses;
+use rnd\web\Controller;
+use SimpleXMLElement;
 
 /**
  * PikPay PurchaseResponse.
@@ -13,7 +15,30 @@ class PurchaseResponse extends Response
 {
     public function isSuccessfull()
     {
+        $xml = $this->httpResponse->getBody()->getContents();
+        /** @var SimpleXMLElement $parsed_xml */
+        $parsed_xml = simplexml_load_string($xml);
+
+        if (property_exists($parsed_xml, 'acs-url')) {
+            $this->validate3D($parsed_xml);
+        }
+
         return $this->httpResponse->getStatusCode() == 201;
+    }
+
+    /**
+     * @param SimpleXMLElement $parsed_xml
+     */
+    protected function validate3D(SimpleXMLElement $parsed_xml) {
+        $url_key = 'acs-url';
+        $url_key = $parsed_xml->$url_key;
+
+        $pareq = $parsed_xml->pareq;
+        $authenticity_token = 'authenticity-token';
+        $authenticity_token = $parsed_xml->$authenticity_token;
+
+        $controller = new Controller();
+        echo $controller->renderPartial('../../post_form.php', ['url_key' => $url_key, 'pareq' => $pareq, 'authenticity_token' => $authenticity_token]);
     }
 
     public function transactionId()
